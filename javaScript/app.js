db5a949b6a7682330e68084e5749c813a70709b3
cosy9 <66667413+cosy9@users.noreply.github.com>
2021-07-19 17:45:48 +0530

    Add files via upload

--- javaScript/app.js
+++ javaScript/app.js
@@ -6,10 +6,10 @@ let addBtn = document.getElementById("addBtn");
 
 addBtn.addEventListener("click", function(e) {
     let addTxt = document.getElementById("addTxt");
-    let addTitle = document.getElementById("title");
-    let today = new Date();
-    let day = today.toString();
-    let currDate = day.substring(0, 25);
+		if(addTxt.value.length === 0){
+			return false
+		}
+		// console.log(addTxt.value);
     let notes = localStorage.getItem("notes"); // during 1st ieration it will be null as it is defined for the first time here notes is an empty key .
     if (notes == null) {
         // alert('please enter a input data');
@@ -18,19 +18,10 @@ addBtn.addEventListener("click", function(e) {
 
         notesObj = JSON.parse(notes);
     }
-
-    let myObj = {
-        title: addTitle.value,
-        text: addTxt.value,
-        date: currDate
-    }
-
-    notesObj.push(myObj);
+    notesObj.push(addTxt.value);
     localStorage.setItem("notes", JSON.stringify(notesObj)); //here during 1st iteration notes key will be set by notesObj which has user inputted text
     addTxt.value = "";
-    addTitle.value = "";
-    currDate = "";
-    // console.log(notesObj);
+    console.log(notesObj);
     showNotes();
 });
 
@@ -41,24 +32,29 @@ function showNotes() {
     } else {
         notesObj = JSON.parse(notes);
     }
+    let dates = localStorage.getItem("dates"); // during 1st ieration it will be null as it is defined for the first time here notes is an empty key .
+    if (dates == null) {
+        // alert('please enter a input data');
+        datesObj = []; // so therefore an noteObj is created compulsaryly.
+    } else {
 
+        datesObj = JSON.parse(dates);
+    }
+    let today = new Date();
+    datesObj.push(today);
+    localStorage.setItem("dates", JSON.stringify(datesObj)); //here during 1st iteration notes key will be set by notesObj which has user inputted text
     let html = "";
-
-
     notesObj.forEach(function(element, index) { //here element,index denotes element value and its index position in array notesObj.
-
 
         html += `<div class="noteCard my-2 mx-2 card" style="width: 18rem">
         <div class="card-body">
-            <h5 class="card-title">${element.title}</h5>
-            <p class="card-text"> ${element.text} </p>
+            <h5 class="card-title">Note ${index + 1}</h5>
+            <p class="card-text"> ${element} </p>
             <hr />
-            <p class="card-text"> ${element.date} </p>
-            <hr />
+            <p>${today} </p>
             <button id = "${index}" onclick="deleteNote(this.id)" class="btn btn-primary">Delete Note</button>
         </div>
     </div>`
-
     });
     let notesElm = document.getElementById("notes");
     if (notesObj.length != 0) {
@@ -71,7 +67,7 @@ function showNotes() {
 //function to delete a note
 
 function deleteNote(index) {
-    // console.log("I am deleting", index);
+    console.log("I am deleting", index);
 
     let notes = localStorage.getItem("notes");
     if (notes == null) {
@@ -93,16 +89,22 @@ search.addEventListener('input', function() {
 
     let inputval = search.value.toLowerCase();
 
-    // console.log('input event fired : ', inputval);
+    console.log('input event fired : ', inputval);
     let noteCards = document.getElementsByClassName('noteCard');
     Array.from(noteCards).forEach(function(element) {
-        let cardTxt = element.getElementsByTagName("p")[0].innerText.toLowerCase();
-        let cardTitle = element.getElementsByTagName("h5")[0].innerText.toLowerCase();
-        if (cardTxt.includes(inputval) || cardTitle.includes(inputval)) {
+        let cardTxt = element.getElementsByTagName("p")[0].innerText;
+        if (cardTxt.includes(inputval)) {
             element.style.display = 'block';
         } else {
             element.style.display = 'none';
         }
     })
 
+})
+
+//preventing submit functionalty
+
+let submitBtn = document.querySelector('.btn-outline-success')
+submitBtn.addEventListener('click',(e)=>{
+	e.preventDefault()
 })
